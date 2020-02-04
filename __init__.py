485a8a41afc08c98c6cde6a552991ab4f28e497c
antoninovara <antoninovara@gmail.com>
2020-02-04 08:12:32 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -37,14 +37,27 @@ class HelloWorldSkill(MycroftSkill):
 
     @intent_handler(IntentBuilder('ThankYouIntent').require('ThankYouKeyword'))
     def handle_thank_you_intent(self, message):
-        """ This is an Adapt intent handler, it is triggered by a keyword."""
-        self.speak_dialog("welcome")
+        """ Skills can log useful information. These will appear in the CLI and
+        the skills.log file."""
+        try:
+            self.speak_dialog("welcome")
+            subprocess.call("sh /home/pi/ir2.sh", shell=True)
+        finally:
+            self.log.info("There are five types of log messages: "
+                          "info, debug, warning, error, and exception.")
+        #self.speak_dialog("welcome")
 
     @intent_handler('HowAreYou.intent')
     def handle_how_are_you_intent(self, message):
-        """ This is a Padatious intent handler.
-        It is triggered using a list of sample phrases."""
-        self.speak_dialog("how.are.you")
+        """ Skills can log useful information. These will appear in the CLI and
+        the skills.log file."""
+        try:
+            self.speak_dialog("how.are.you")
+            subprocess.call("sh /home/pi/ir3.sh", shell=True)
+        finally:
+            self.log.info("There are five types of log messages: "
+                          "info, debug, warning, error, and exception.")
+        #self.speak_dialog("how.are.you")
 
     @intent_handler(IntentBuilder('HelloWorldIntent')
                     .require('HelloWorldKeyword'))
